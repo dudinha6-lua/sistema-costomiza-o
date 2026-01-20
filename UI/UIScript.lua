b82f548658e63b6d126a935ef57d7c6d5ade43f6
dudinha6-lua <eduarda.rischtter2007@gmail.com>
2026-01-20 13:05:23 -0300

    Update UIScript.lua

--- UI/UIScript.lua
+++ UI/UIScript.lua
@@ -1,1 +1,21 @@
+-- UIScript.lua
+local SkinSystem = require(game.ReplicatedStorage.Scripts.SkinSystem)
+local ShopSystem = require(game.ReplicatedStorage.Scripts.ShopSystem)
+
+local playerCoins = 500
+
+-- Exemplo de botões (ajuste os nomes conforme sua UI)
+local equipButton = script.Parent.EquipButton
+local buyButton = script.Parent.BuyButton
+
+equipButton.MouseButton1Click:Connect(function()
+	SkinSystem:equipSkin("SkinExemplo")
+end)
+
+buyButton.MouseButton1Click:Connect(function()
+	local success = ShopSystem:buyItem("SkinExemplo", 200, playerCoins)
+	if success then
+		playerCoins -= 200
+	end
+end)
 
