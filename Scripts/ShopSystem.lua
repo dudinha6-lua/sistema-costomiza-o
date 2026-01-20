0faf56b0b0186869e8252d12c98a0117cfa48b4f
dudinha6-lua <eduarda.rischtter2007@gmail.com>
2026-01-20 13:04:32 -0300

    Update ShopSystem.lua

--- Scripts/ShopSystem.lua
+++ Scripts/ShopSystem.lua
@@ -1,1 +1,22 @@
+-- ShopSystem.lua
+local ShopSystem = {}
+
+local ownedItems = {}
+
+function ShopSystem:buyItem(itemName, price, playerCoins)
+	if playerCoins < price then
+		warn("Moedas insuficientes")
+		return false
+	end
+
+	ownedItems[itemName] = true
+	print("Item comprado:", itemName)
+	return true
+end
+
+function ShopSystem:hasItem(itemName)
+	return ownedItems[itemName] == true
+end
+
+return ShopSystem
 
