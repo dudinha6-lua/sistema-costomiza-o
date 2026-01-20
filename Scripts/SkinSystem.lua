74f990321a9d85b003429ae53258f19674cb23ef
dudinha6-lua <eduarda.rischtter2007@gmail.com>
2026-01-20 13:03:08 -0300

    Update SkinSystem.lua

--- Scripts/SkinSystem.lua
+++ Scripts/SkinSystem.lua
@@ -1,1 +1,29 @@
+-- SkinSystem.lua
+local SkinSystem = {}
+
+-- Skin atualmente equipada
+local equippedSkin = nil
+
+function SkinSystem:equipSkin(skinName)
+	if equippedSkin == skinName then
+		warn("Skin já equipada")
+		return
+	end
+
+	equippedSkin = skinName
+	print("Skin equipada:", skinName)
+end
+
+function SkinSystem:removeSkin()
+	if equippedSkin then
+		print("Skin removida:", equippedSkin)
+		equippedSkin = nil
+	end
+end
+
+function SkinSystem:getEquippedSkin()
+	return equippedSkin
+end
+
+return SkinSystem
 
